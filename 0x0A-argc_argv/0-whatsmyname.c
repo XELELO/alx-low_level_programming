2410f6919bcee818815935f0fb8258d4b6757790
Yamkela <104762419+XELELO@users.noreply.github.com>
2022-07-04 08:12:25 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  * main -> prints its name, followed by a new line.
-  * @argc: number of command line arguments.
-  * @argv: array that contains the program command line arguments.
-  * Return: 0 - success.
+  * main -> this is a function to print its name
+  * @argc: argc parameter
+  * @argv: an array of a command listed
+  * Return: 0 for success.
  */
 int main(int argc, char *argv[])
 {
